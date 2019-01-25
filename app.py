a3ac65f3a24a1b3df1330c4908b34c278b550baf
Mackenzie Toner <mackenzie.toner@unb.ca>
2019-01-25 09:11:07 -0400

    read dataframes

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request, jsonify
 from tinydb import TinyDB, Query
+import pandas as pd
 
 app = Flask(__name__)
 db = TinyDB('data.json')
+df_turbines = pd.read_excel('aec.xlsx', nrows=4)
+df_optimal_costs = pd.read_excel('aec.xlsx', skiprows=11).dropna(axis=1)
+
 
 
 @app.route('/app')
 def hello_world():
     q = Query()
-
-    result = (db.search(q.thing == 'val2'))
-    for element in result:
-        print(element['thing'])
+    print(df_turbines)
+    print(df_optimal_costs)
     return render_template('app.html')
 
 @app.route( '/app/addEvent', methods=['POST'] )
