1b22e6111994fa36fe0b82298b85d4ce1f1d3de9
Seth Schilbe <seth.schilbe@gmail.com>
2019-01-25 15:12:29 -0400

    Fix data updates

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, send_from_directory, jsonify
-from tinydb import TinyDB, Query
 from data_processing import *
 import os
 
 app = Flask(__name__)
-db = TinyDB('data.json')
 dataProcessor = DataProcessor()
 dataProcessor.calculate_cost(100000000, 'Type 1')
 
@@ -12,43 +10,56 @@ dataProcessor.calculate_cost(100000000, 'Type 1')
 def hello_world():
     return render_template('app.html')
 
-@app.route( '/app/addEvent', methods=['POST'] )
-def add_event():
-    db.insert( request.get_json() )
-    return jsonify(success=True)
-
-@app.route('/app/fetch')
-def getEvents():
-    query = Query()
-
-    result = db.all()
-    return jsonify(result)
-
 @app.route('/app/submit',  methods=['POST'] )
 def submit():
     requestType = request.form['type']
     if requestType == 'Calculate':
         json_turbine = {}
         json_optional = {}
+
+        COLUMN_INDEX = 0 # Represents the index for accessing a column ex. Cut-in wind speed
+        TYPE_INDEX = 1 # Represents the type the data represents (turbine or optional costs)
+        ROW_INDEX = 2 # Represents which turbine or optional cost this data is referencing
+        
         for value_in in request.form:
             split = value_in.split('_')
-            print(split[0])
+            
             if len(split) < 3: # represent budget or type 
-                if split[0] == 'budget':
-                    budget = request.form[split[0]]
-            elif split[1] == 'turbine': # represents data for the turbines
-                if split[2] not in json_turbine:
-                    json_turbine[split[2]] = {}
-                else:
-                    json_turbine[split[2]][split[0]] = request.form[split[0]]
-            else: # represents data for the optional costs
-                if split[2] not in json_optional and split[2].isdigit():
-                    json_optional[split[2]] = {}
-                else:
-                    json_optional[split[2]][split[0]] = request.form[split[0]]
+                if split[COLUMN_INDEX] == 'budget':
+                    budget = request.form[split[COLUMN_INDEX]]
+                elif split[COLUMN_INDEX] == 'profit':
+                    price_per_kwh = request.form[split[COLUMN_INDEX]]
 
-        print(json_turbine)
-        print(json_optional)
+            elif split[TYPE_INDEX] == 'turbine': # represents data for the turbines
+                if split[ROW_INDEX] not in json_turbine:
+                    json_turbine[split[ROW_INDEX]] = {}
+                else:
+                    json_turbine[split[ROW_INDEX]][split[COLUMN_INDEX]] = request.form[value_in]
+
+            else: # represents data for the optional costs
+                if split[ROW_INDEX] not in json_optional:
+                    json_optional[split[ROW_INDEX]] = {}
+                else:
+                    json_optional[split[ROW_INDEX]][split[COLUMN_INDEX]] = request.form[value_in]
+
+        # Modify Turbine information tables
+        for turbine in json_turbine:
+            row = dataProcessor.df_turbines.loc[dataProcessor.df_turbines['Turbine Type'] == turbine]
+            
+            for attribute in json_turbine[turbine]:
+                row[attribute].iloc[0] = json_turbine[turbine][attribute]
+
+            dataProcessor.df_turbines.loc[dataProcessor.df_turbines['Turbine Type'] == turbine] = row
+
+        # Modify Optional Cost information tables
+        for option in json_optional:
+            row = dataProcessor.df_optional_costs.loc[dataProcessor.df_optional_costs['Type'] == option]
+            
+            for attribute in json_optional[option]:
+                row[attribute].iloc[0] = json_optional[option][attribute]
+
+            dataProcessor.df_optional_costs.loc[dataProcessor.df_optional_costs['Type'] == option] = row
+
         return "Perform a calculation!"
     elif requestType == 'Export':
         return "Perform an export!"
