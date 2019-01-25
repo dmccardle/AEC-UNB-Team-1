2793750e57fcdcaf3709a7697a49e57b99701634
dmccardle <danny.mccardle@unb.ca>
2019-01-25 16:57:23 -0400

    Refactored form processing out to its own function

--- app.py
+++ app.py
@@ -26,30 +26,8 @@ def submit():
         # stores json data about each optional data type from the excel document
         json_optional = {}
 
-        COLUMN_INDEX = 0 # Represents the index for accessing a column ex. Cut-in wind speed
-        TYPE_INDEX = 1 # Represents the type the data represents (turbine or optional costs)
-        ROW_INDEX = 2 # Represents which turbine or optional cost this data is referencing
-        
-        for value_in in request.form:
-            split = value_in.split('_')
-
-            if len(split) < 3: # represent budget or type 
-                if split[COLUMN_INDEX] == 'budget':
-                    budget = request.form[split[COLUMN_INDEX]]
-                elif split[COLUMN_INDEX] == 'profit':
-                    price_per_kwh = request.form[split[COLUMN_INDEX]]
-
-            elif split[TYPE_INDEX] == 'turbine': # represents data for the turbines
-                if split[ROW_INDEX] not in json_turbine:
-                    json_turbine[split[ROW_INDEX]] = {}
-                else:
-                    json_turbine[split[ROW_INDEX]][split[COLUMN_INDEX]] = request.form[value_in]
-
-            else: # represents data for the optional costs
-                if split[ROW_INDEX] not in json_optional:
-                    json_optional[split[ROW_INDEX]] = {}
-                else:
-                    json_optional[split[ROW_INDEX]][split[COLUMN_INDEX]] = request.form[value_in]
+        # process which form data is for the turbine, and which is for the optional data
+        json_turbine, json_optional = process_form_data()
 
         # Modify Turbine information tables
         for turbine in json_turbine:
@@ -68,7 +46,7 @@ def submit():
                 row[attribute].iloc[0] = json_optional[option][attribute]
 
             dataProcessor.df_optional_costs.loc[dataProcessor.df_optional_costs['Type'] == option] = row
-
+            
         return "Perform a calculation!"
     elif requestType == 'Export':
         return "Perform an export!"
@@ -100,3 +78,39 @@ def get_result():
 
 if __name__ == '__main__':
     app.run()
+
+def process_form_data():
+    # Format of value data: 
+    COLUMN_INDEX = 0 # Represents the index for accessing a column ex. Cut-in wind speed
+    TYPE_INDEX = 1 # Represents the type the data represents (turbine or optional costs)
+    ROW_INDEX = 2 # Represents which turbine or optional cost this data is referencing
+    
+    # variables to store return values
+    turbine_return_json = {}
+    optional_return_json = {}
+
+    for value in request.form:
+        # Split the input value to allow for accessing each element in the string
+        value_split = value.split('_')
+
+        # If there are less than three strings, the data represents information about either 
+        # the budget of the price per kwh
+        if len(value_split) < 3:
+            if value_split[COLUMN_INDEX] == 'budget':
+                budget = request.form[value_split[COLUMN_INDEX]]
+            elif value_split[COLUMN_INDEX] == 'profit':
+                price_per_kwh = request.form[value_split[COLUMN_INDEX]]
+        # Represents a value containing turbine information
+        elif value_split[TYPE_INDEX] == 'turbine':
+            if value_split[ROW_INDEX] not in turbine_return_json:
+                turbine_return_json[value_split[ROW_INDEX]] = {}
+            else:
+                turbine_return_json[value_split[ROW_INDEX]][value_split[COLUMN_INDEX]] = request.form[value]
+        # Represents a value containing optional data information
+        else:
+            if value_split[ROW_INDEX] not in optional_return_json:
+                optional_return_json[value_split[ROW_INDEX]] = {}
+            else:
+               optional_return_json[value_split[ROW_INDEX]][value_split[COLUMN_INDEX]] = request.form[value]
+        
+    return turbine_return_json, optional_return_json
