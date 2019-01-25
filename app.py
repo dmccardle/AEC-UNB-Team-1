fd3b4c954489dfd6fe774873760d00d0e299db71
Seth Schilbe <seth.schilbe@gmail.com>
2019-01-25 15:12:29 -0400

    Fix data updates

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def submit():
     if requestType == 'Calculate':
         json_turbine = {}
         json_optional = {}
+
         for value_in in request.form:
             split = value_in.split('_')
-            print(split[0])
             if len(split) < 3: # represent budget or type 
                 if split[0] == 'budget':
                     budget = request.form[split[0]]
@@ -40,15 +40,29 @@ def submit():
                 if split[2] not in json_turbine:
                     json_turbine[split[2]] = {}
                 else:
-                    json_turbine[split[2]][split[0]] = request.form[split[0]]
+                    json_turbine[split[2]][split[0]] = request.form[value_in]
             else: # represents data for the optional costs
-                if split[2] not in json_optional and split[2].isdigit():
+                if split[2] not in json_optional:
                     json_optional[split[2]] = {}
                 else:
-                    json_optional[split[2]][split[0]] = request.form[split[0]]
+                    json_optional[split[2]][split[0]] = request.form[value_in]
 
-        print(json_turbine)
-        print(json_optional)
+        # Modify Turbine information tables
+        for turbine in json_turbine:
+            row = dataProcessor.df_turbines.loc[dataProcessor.df_turbines['Turbine Type'] == turbine]
+            for attribute in json_turbine[turbine]:
+                row[attribute].iloc[0] = json_turbine[turbine][attribute]
+
+            dataProcessor.df_turbines.loc[dataProcessor.df_turbines['Turbine Type'] == turbine] = row
+
+        # Modify Optional Cost information tables
+        for option in json_optional:
+            row = dataProcessor.df_optional_costs.loc[dataProcessor.df_optional_costs['Type'] == option]
+            for attribute in json_optional[option]:
+                row[attribute].iloc[0] = json_optional[option][attribute]
+
+            dataProcessor.df_optional_costs.loc[dataProcessor.df_optional_costs['Type'] == option] = row
+
         return "Perform a calculation!"
     elif requestType == 'Export':
         return "Perform an export!"
