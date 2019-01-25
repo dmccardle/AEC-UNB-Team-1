aafc1ae1fd3d3657de58440f50a164ce0ab4e48c
Mackenzie Toner <mackenzie.toner@unb.ca>
2019-01-25 14:43:17 -0400

    user docs and remove tinydb

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
 from flask import Flask, render_template, request, send_from_directory, jsonify
-from tinydb import TinyDB, Query
 from data_processing import *
 import os
 
 app = Flask(__name__)
-db = TinyDB('data.json')
 dataProcessor = DataProcessor()
 dataProcessor.calculate_cost(100000000, 'Type 1')
 
 @app.route('/app')
 def hello_world():
     return render_template('app.html')
-
-@app.route( '/app/addEvent', methods=['POST'] )
-def add_event():
-    db.insert( request.get_json() )
-    return jsonify(success=True)
-
-@app.route('/app/fetch')
-def getEvents():
-    query = Query()
-
-    result = db.all()
-    return jsonify(result)
 
 @app.route('/app/submit',  methods=['POST'] )
 def submit():
--- data_processing.py
+++ data_processing.py
@@ -4,7 +4,7 @@ from statistics import stdev
 class DataProcessor():
     def __init__(self):
         self.df_turbines = pd.read_excel('aec.xlsx', nrows=4)
-        self.df_optimal_costs = pd.read_excel('aec.xlsx', skiprows=11).dropna(axis=1)
+        self.df_optional_costs = pd.read_excel('aec.xlsx', skiprows=11).dropna(axis=1)
         self.df_wind_data = pd.read_excel('aec.xlsx', sheet_name='wind-data', index_col=0)
         self.df_depth_data = pd.read_excel('aec.xlsx', sheet_name='depth-data', index_col=0)
         tmp = self.df_wind_data.values.reshape(1, -1)[0]  # flatten data points
