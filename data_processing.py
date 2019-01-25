e907c484fcabc84a15725e7e34350ed0b1a0daca
Mackenzie Toner <mackenzie.toner@unb.ca>
2019-01-25 11:35:24 -0400

    Fix std dev

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from statistics import stdev
 
 class DataProcessor():
     def __init__(self):
@@ -6,7 +7,9 @@ class DataProcessor():
         self.df_optimal_costs = pd.read_excel('aec.xlsx', skiprows=11).dropna(axis=1)
         self.df_wind_data = pd.read_excel('aec.xlsx', sheet_name='wind-data', index_col=0)
         self.df_depth_data = pd.read_excel('aec.xlsx', sheet_name='depth-data', index_col=0)
-        self.wind_stdev = self.df_wind_data.std()
+        tmp = self.df_wind_data.values.reshape(1, -1)[0]  # flatten data points
+        tmp = [x for x in tmp if isinstance(x, (int, float))]  # remove non-numbers
+        self.wind_stdev = stdev(tmp)
 
     def find_location_candidates(self, nominal_speed):
         for a in self.df_wind_data.itertuples():
